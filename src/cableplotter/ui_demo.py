a7432fc5e48743f2762382c4f3f10aa0db1b902c
JamahaW <pret2titio1n@gmail.com>
2024-07-18 18:35:56 +0300

    matplotlib ui demo ref

--- src/cableplotter/ui_demo.py
+++ src/cableplotter/ui_demo.py
@@ -2,36 +2,11 @@ from typing import Callable
 from typing import Optional
 
 import matplotlib.pyplot
-# Импортируем класс слайдера
 from matplotlib.widgets import Slider
 from numpy import exp
 from numpy import linspace
 from numpy import pi
 from numpy import sqrt
-
-
-class Gauss:
-
-    def __init__(self):
-        pass
-
-
-def gaussian(sigma, mu, x):
-    return 1.0 / (sigma * sqrt(2.0 * pi)) * exp(-((x - mu) ** 2) / (2 * sigma ** 2))
-
-
-def updateGraph(v: float):
-    global slider_sigma
-    global slider_mu
-    global graph_axes
-
-    # Используем атрибут val, чтобы получить значение слайдеров
-    x = linspace(-5.0, 5.0, 300)
-    y = gaussian(slider_sigma.val, slider_mu.val, x)
-
-    graph_axes.clear()
-    graph_axes.plot(x, y)
-    matplotlib.pyplot.draw()
 
 
 def createSlider(
@@ -64,17 +39,38 @@ def createSlider(
     return s
 
 
+def gaussian(sigma, mu, x):
+    return 1.0 / (sigma * sqrt(2.0 * pi)) * exp(-((x - mu) ** 2) / (2 * sigma ** 2))
+
+
+class Gauss:
+
+    def __init__(self):
+        SLIDER_SIZE = (0.8, 0.05)
+        SLIDER_VALUES = (0.01, 1.0)
+
+        fig, self.graph_axes = matplotlib.pyplot.subplots()
+        self.graph_axes.grid()
+
+        fig.subplots_adjust(left=0.07, right=0.95, top=0.95, bottom=0.4)
+
+        self.slider_sigma = createSlider('σ', (0.25, 0.05), SLIDER_SIZE, SLIDER_VALUES, self.__updateGraph)
+        self.slider_mu = createSlider('μ', (0.17, 0.05), SLIDER_SIZE, SLIDER_VALUES, self.__updateGraph)
+
+        self.__updateGraph(0)
+
+    @staticmethod
+    def run():
+        matplotlib.pyplot.show()
+
+    def __updateGraph(self, _: float):
+        x = linspace(-5.0, 5.0, 300)
+        y = gaussian(self.slider_sigma.val, self.slider_mu.val, x)
+
+        self.graph_axes.clear()
+        self.graph_axes.plot(x, y)
+        matplotlib.pyplot.draw()
+
+
 if __name__ == '__main__':
-    SLIDER_SIZE = (0.8, 0.05)
-    SLIDER_VALUES = (0.01, 1.0)
-
-    fig, graph_axes = matplotlib.pyplot.subplots()
-    graph_axes.grid()
-
-    fig.subplots_adjust(left=0.07, right=0.95, top=0.95, bottom=0.4)
-
-    slider_sigma = createSlider('σ', (0.25, 0.05), SLIDER_SIZE, SLIDER_VALUES, updateGraph)
-    slider_mu = createSlider('μ', (0.17, 0.05), SLIDER_SIZE, SLIDER_VALUES, updateGraph)
-
-    updateGraph(0)
-    matplotlib.pyplot.show()
+    Gauss().run()
